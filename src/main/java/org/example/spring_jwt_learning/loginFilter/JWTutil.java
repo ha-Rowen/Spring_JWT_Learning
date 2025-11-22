39ffdf4b2fc3338af96369b8cf12d87cdab3933a
DESKTOP-RNU182K\김종학 <jonghak.re@gmail.com>
2025-11-22 09:15:07 +0900

    update JWTutil,JWTutilTest

--- src/main/java/org/example/spring_jwt_learning/loginFilter/JWTutil.java
+++ src/main/java/org/example/spring_jwt_learning/loginFilter/JWTutil.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Encoders;
+import org.example.spring_jwt_learning.Entity.UserEntity;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -17,39 +18,44 @@ import java.util.Date;
 @Component
 public class JWTutil {
 
-    protected String    jwtId;
-    protected SecretKey  keys;  // 테스트 코드에서 객체를 상속받고 변수 접근을 하기 위해서 protected를 사용
-    protected String Encodeds; // 테스트 코드에서 객체를 상속받고 변수 접근을 하기 위해서 protected를 사용
-    protected SecretKey  secretKey;
-    public JWTutil (@Value("${String.TEST.JWT.KEY}")String keys )
-    {
-       this.keys     = Jwts.SIG.HS256.key().build();
-       this.jwtId    = java.util.UUID.randomUUID().toString();
-       /// 이거 createJwt 메서드로 가야하나???
-        // 아직 JWTutil의 활동 주기를 모르니까 확인하고 id는 매번 새롭게 필요한건지 확인하고 메서드에 넣기로 하자
-       this.Encodeds = Encoders.BASE64.encode(this.keys.getEncoded());
+    protected String         jwtId;  // 솔직히 이게 아직 왜 쓸모있는지 모르겠다. 일단 계속 진행해보자
+    protected SecretKey       keys;  // 테스트 코드에서 객체를 상속받고 변수 접근을 하기 위해서 protected를 사용
+    protected String      Encodeds;// 테스트 코드에서 객체를 상속받고 변수 접근을 하기 위해서 protected를 사용
 
-        secretKey = new SecretKeySpec(keys.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());// test용이다
+   // @Value("${String.TEST.JWT.KEY}")
+    protected String          SpringKey="vmfhaltmskdlstkfkdgodyroqkfwkdbalroqkfwkdbalaaaaaaaaaaaaaaaabbbbb";
+
+    public JWTutil ( )
+    {
+        this.keys = Jwts.SIG.HS256.key().build();;
+       this.jwtId    = java.util.UUID.randomUUID().toString();
+       this.Encodeds = Encoders.BASE64.encode(this.keys.getEncoded()); // 검증로직에서 활용하기 위함
+
+       // secretKey = new SecretKeySpec(keys.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+        //이렇게 직접 작업하지 않아도  .signWith(this.keys)를 하면 알아서 라이브러리가 Base64 및 암호화를 전부 진행해줘서 괜찮음.
+        // 0.12.3 버전에는 hmacShaKeyFor를 지원하지 않아서 어쩔 수 없다.
     }
+
 
 
     public String getUsername(String token)
     {
-        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("username", String.class);
+        return Jwts.parser().verifyWith(keys).build().parseSignedClaims(token) //말 그대로 파싱과정
+                .getPayload().get("username", String.class);
        // 예외처리를 하는게 좋다고 한다. 예외처리는 자원을 많이 소비하는 걸로 알고 있는데 여기서는 뭐.. 쓰는게 좋아 보이는데...
     }
 
 
-    public String createJwt(String username, String role, Long expiredMs)
+    public String createJwt(UserEntity user , Long expiredMs)
     {
         return Jwts
                 .builder()
                 .id(jwtId)
-                .claim("username",username)
-                .claim("role",role)
+                .claim("UserName",user.getName())
+                .claim("Role",user.getRole())
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + expiredMs))
-                .signWith(this.keys)
+                .signWith(keys)
                 .compact();
     }
 
--- src/test/java/org/example/spring_jwt_learning/JWTutilTest.java
+++ src/test/java/org/example/spring_jwt_learning/JWTutilTest.java
@@ -1,18 +1,16 @@
 package org.example.spring_jwt_learning;
 
-import com.fasterxml.jackson.annotation.JsonGetter;
-import lombok.Getter;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import org.example.spring_jwt_learning.Entity.UserEntity;
 import org.example.spring_jwt_learning.loginFilter.JWTutil;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.crypto.SecretKey;
-
-import java.beans.Encoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,16 +18,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith(MockitoExtension.class)
 public class JWTutilTest {
     // 스프링 컨테이너가 생성한 JWTutil 인스턴스를 주입받습니다.
-    private JwtTestClass jwtUtil  = new JwtTestClass()     ;
 
-    private SecretKey kets        = jwtUtil.GetKeys()      ;
+    private  JwtTestClass jwtUtil  = new JwtTestClass()    ;
+    private SecretKey keys        = jwtUtil.GetKeys()      ;
     private String    Encodeds    = jwtUtil.GetEncodeds()  ;
     private String    jwtid       = jwtUtil.GetJwtId()     ;
 
+
+
+    private UserEntity user = UserEntity.builder()
+                                 .name("김철수")
+                                    .role("User")
+                                        .email("12345798@naver.com")
+                                             .password("dfafa")
+                                            .build();
+
     @Test
+    @DisplayName("JWT키값 검증")
     void keyInitializationTest() {
         // 1. 키와 코드가 null이 아닌지 확인
-        assertThat(kets)
+        assertThat(keys)
                 .isNotNull();
 
         assertThat(Encodeds)
@@ -37,20 +45,44 @@ public class JWTutilTest {
 
         assertThat(jwtid)
                 .isNotNull();
+
+        assertThat(jwtUtil)
+                .isNotNull();
         // 데이터 출력 해보기
-        System.out.println("Injected Key: " + kets);
+        System.out.println("Injected Key: " + keys);
         System.out.println("Injected Encoded: " + Encodeds);
         System.out.println("Injected JwtId: "+ jwtid);
+        System.out.println("Injected jwtUtil: "+ jwtUtil);
+
+
+
+    }
+
+    @Test
+    @DisplayName("JWT 생성 및 파싱 검증")
+    void Parse()
+    {
+
+       String JWT= jwtUtil.createJwt(user,1200L);
+
+      Jws<Claims> claims = Jwts.parser()
+                .verifyWith(this.keys)
+                .build()
+                .parseSignedClaims(JWT);
+
+        assertThat(claims.getPayload().getId()).isEqualTo(this.jwtid);
+        assertThat(claims.getPayload().get("UserName")).isEqualTo("김철수");
+        assertThat(claims.getPayload().get("Role")).isEqualTo("User");
 
     }
 }
 
 
-class JwtTestClass extends JWTutil{
+class JwtTestClass extends JWTutil {
 
-    public JwtTestClass(String keys) {
-        super(keys);
-    }
+
+
+
 
     public SecretKey GetKeys()
    {
@@ -66,4 +98,5 @@ class JwtTestClass extends JWTutil{
    {
        return jwtId;
    }
+
 }
