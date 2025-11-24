a6c2579ab0f43b58241f956559b9bf857ae7f4ca
DESKTOP-RNU182K\김종학 <jonghak.re@gmail.com>
2025-11-24 23:27:15 +0900

    .

--- src/test/java/org/example/spring_jwt_learning/SpringJwtLearningApplicationTests.java
+++ src/test/java/org/example/spring_jwt_learning/SpringJwtLearningApplicationTests.java
@@ -45,9 +45,9 @@ class SpringJwtLearningApplicationTests {
         Roles.add("moderator");
         user = UserEntity.builder()
                 .name(RS.RandomString())
-                .email("test19@gmail.com")// DB에 보조키로 설정되어있어서 항상 다른 값이어야한다.
-                .password("744sss5789s1") // 중복이 안됀다. 계속 바꿔줘야 한다.
-                .roles(Roles) // DB에 아직 set자료 형으로 받아오는 로직을 수정하지 않았다.
+                .email("test20@gmail.com")
+                .password("744sss5789s1")
+                .roles(Roles)
                 .build();
 
         System.out.println(user.getRoles());
@@ -61,9 +61,6 @@ class SpringJwtLearningApplicationTests {
         // DB 연결 시도
         try (Connection connection = dataSource.getConnection()) {
             Assertions.assertNotNull(connection);
-            System.out.println("DB 연결 성공!");
-            System.out.println("DB URL: " + connection.getMetaData().getURL());
-            System.out.println("DB 사용자: " + connection.getMetaData().getUserName());
         }
     }
 
@@ -71,7 +68,7 @@ class SpringJwtLearningApplicationTests {
     @DisplayName("DB_ADD_TEST")
     @Order(2)
     void DB_add_TEST() {
-        //JR.add(user); // test 할때마다 회원이 등록되서 필요할 때, test한다.
+        JR.add(user);
     }
 
     @Test
